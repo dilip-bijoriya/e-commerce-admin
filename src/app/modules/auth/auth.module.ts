52191894bd5a8957e8e045e270b5dd6002dd2dfd
dilip-bijoriya <dilipbijoriya6588@gmail.com>
2023-06-01 23:12:34 +0530

    project setup

---
+++ src/app/modules/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthComponent } from './auth.component';
+import { LoginComponent } from './components/login/login.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    AuthComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    ResetPasswordComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    SharedModule
+  ]
+})
+export class AuthModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedRoutingModule } from './shared-routing.module';
+import { SharedComponent } from './shared.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { InputComponent } from './components/input/input.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    SharedComponent,
+    HeaderComponent,
+    SidebarComponent,
+    InputComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [HeaderComponent, SidebarComponent, InputComponent]
+})
+export class SharedModule { }
