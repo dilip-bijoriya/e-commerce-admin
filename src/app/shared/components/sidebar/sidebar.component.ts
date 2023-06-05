65d9cef68aeb2d3928049a2b041d011a18637a98
dilipbijoriya6588@gmail.com <dilipbijoriya6588@gmail.com>
2023-06-05 18:58:51 +0530

    navigation issue fixed

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 declare var $: any;
 @Component({
   selector: 'app-sidebar',
@@ -6,12 +7,12 @@ declare var $: any;
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-  constructor() { }
+  constructor(private router: Router) { }
 
   sidebarData: Array<any> = [
     {
       name: 'Inventry',
-      path: '',
+      path: '/dashboard/inventry',
       icon: 'icon-house-outline-2'
     },
     {
@@ -36,22 +37,10 @@ export class SidebarComponent implements OnInit {
     },
   ]
   ngOnInit(): void {
-    // $(".logo_for_sidebar").click(function(){
-    //   $(".sidebar_wrap").toggleClass("menu-hide submenu-hide");
-    // });
-    // $(".nav-item").click(function(){
-    //   $(".sidebar_wrap").removeClass("menu-hide submenu-hide");
-    // });
-    // $(".sidebar-sticky li").click(function(){
-    //   $("li.active").removeClass("active");
-    //   $(this).addClass("active");
-    //   $(".sidebar_sub").show();
-    // });
-    //   $(document).mouseup(function (e) {
-    //     var sidebarhide = $(".sidebar_sub");
-    //     if (!$('.sidebar_sub').is(e.target) && !sidebarhide.is(e.target) && sidebarhide.has(e.target).length == 0) {
-    //       sidebarhide.hide();
-    //     }
-    //   }); 
+  }
+
+  navigationClick(item: string): void {
+    console.log(item, "DILIP");
+    this.router.navigate([item]);
   }
 }
